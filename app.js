c967c553ed8b2599c2dad55c9f79c0bae55c4f62
Etienne Bourganel <etienne.bourganel@gmail.com>
2021-06-03 10:10:10 +0200

    Add error if insufficient provided login info

--- app.js
+++ app.js
@@ -4,6 +4,17 @@ const path = require("path")
 
 app.set("view engine", "pug")
 app.set("views", path.join(__dirname, "views"))
-
-app.get("/", (req, res) => res.render("index"))
+app.use(express.urlencoded({ extended: true }))
 app.listen(3000, () => console.log("Server ready"))
+app.get("/", (req, res) => res.render("index"))
+app.post("/", (req, res) => {
+  const { username, password } = req.body
+  if (!username || !password) {
+    res.render("error", {
+      message: "Please set both username and password",
+    })
+    return
+  }
+  console.log(req.body, username, password)
+  res.end()
+})

47b1667817e410bbe63775b1caca83711be54df3
Etienne Bourganel <etienne.bourganel@gmail.com>
2021-06-03 09:33:40 +0200

    Add pug template and basic structure

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const app = express()
+const path = require("path")
 
-app.get("/", (req, res) => res.send("Hello World!"))
+app.set("view engine", "pug")
+app.set("views", path.join(__dirname, "views"))
+
+app.get("/", (req, res) => res.render("index"))
 app.listen(3000, () => console.log("Server ready"))
